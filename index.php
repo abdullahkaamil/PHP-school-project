6bfa58abb94f21968b94e97bc90b22e24f722023
akaam <akaamil@outlook.com>
2018-09-24 16:27:35 +0300

    paging done

--- index.php
+++ index.php
@@ -1,57 +1,71 @@
 <html>
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>MY first PHP project</title>
-</head>
-<?php
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ <title>MY first PHP project</title> </head>
+ <body>
+<?php 
 echo "
-    <pre>";
+<pre>";
 print_r($_GET);
 echo "</pre>";
+//switching between fucntions
 if (!isset($_GET['is']))
     $_GET['is'] = '';
-
 switch ($_GET['is']) {
-    case 'sil':
-        sil();
-        listele();
+    case 'detele':
+    detele();
+        listed();
         break;
-    case 'yeni':
-        yeni();
+    case 'newStd':
+        newStd();
         break;
-    case 'ekle':
+    case 'add':
         checking();
-        listele();
+        listed();
         break;
     case 'degistir':
         form();
         break;
-    case 'guncelle':
-        guncelle();
-        listele();
+    case 'update':
+        update();
+        listed();
         break;
     default:
-        listele();
+        listed();
 }
-
-
 // update
-function guncelle()
+function update()
 {
-    $c = mysqli_connect('localhost', 'root', '', 'okul');
-    $sql = "UPDATE ogrenci SET adi='{$_GET['ad']}',soyadi='{$_GET['soyad']}' WHERE ono={$_GET['no']};";
-    //echo $sql;exit;
-    $sonuc = mysqli_query($c, $sql);
+    $c = mysqli_connect('localhost', 'root', '', 'okul'); //sql connect
+    $sql = "UPDATE ogrenci SET adi='{$_GET['ad']}',soyadi='{$_GET['soyad']}' WHERE ono={$_GET['no']};"; //sql update query
+    $result = mysqli_query($c, $sql);
     if (mysqli_affected_rows($c) == 0)
         echo "Kayit bulunamadi (no={$_GET['no']})";
     mysqli_close($c);
 }
-function form()
-{
-    //update form
-    ?>
-<body>
-<h4>Ogrenci Guncelleme</h4>
+ //checking if the student no is exist or not 
+ function checking()
+ {
+     $c = mysqli_connect('localhost', 'root', '', 'okul');
+     if (isset($_GET['no']) && isset($_GET['ad']) && isset($_POST['soyad']));
+     {
+         $no = $_GET['no'];
+         $ad = $_GET['ad'];
+         $soyad = $_GET['soyad'];
+ 
+         $sql = "SELECT* FROM ogrenci where ono = '$no' ";
+         $result = mysqli_query($c, $sql);
+         if (mysqli_num_rows($result) > 0) {
+             echo 'Username already Exists!!';
+         } else {
+             add();
+         }
+     }
+ }
+
+ //updating form
+function form (){?>
+<h4>OgrenGuncellemeci </h4>
 <div class='form-group col-md-8'>
 <form action=''>
 <input type=hidden name=no value='<?php echo $_GET['no']; ?>'>
@@ -75,26 +89,36 @@ function form()
 </table>
 </form>
 </div>
-<?php 
+<?php
 }
-    //adding
-function ekle()
+//adding
+function add()
 {
     $c = mysqli_connect('localhost', 'root', '', 'okul');
     $sql = "INSERT INTO ogrenci(ono,adi,soyadi) VALUES(" . $_GET['no'] . ",'" . $_GET['ad'] . "','" . $_GET['soyad'] . "');";
     echo $sql . "<br>";
-    $sonuc = mysqli_query($c, $sql) or mysqli_error($c);
-    echo $sonuc;
+    $result = mysqli_query($c, $sql) or mysqli_error($c);
+    echo $result;
     mysqli_close($c);
-    //header("location:index.php");
 }
-
-function yeni()
+ //detele
+ function detele()
+ {
+     $c = mysqli_connect('localhost', 'root', '', 'okul'); // VT'ye baglan
+     $sql = "DELETE FROM ogrenci WHERE ono=" . $_GET['no'] . ";";
+     $result = mysqli_query($c, $sql); // SQL komutunu calistir
+     if (!$result) // komutu calistirirken hata olustumu?
+     echo "SQL error:" . mysqli_error($c);
+     mysqli_close($c); // VT baglantisini kapat
+     //header("location:index.php"); // browser'in ogrenci.php sayfasini yuklemesini sagla
+ }
+// 
+function newStd()
 {
     echo "
     <form>
-        Yeni Ogrenci
-        <input type=hidden name=is value=ekle>
+        NEW STUDENT
+        <input type=hidden name=is value=add>
         <div class='form-group row col-md-8'>
             <label for='inputno' class='col-sm-3 col-form-label'>
                 NO: </lable>
@@ -133,45 +157,17 @@ function yeni()
                 </div>
             </div>
         </fieldset>
-        <button type='submit' class='btn btn-primary' name=gonder> Olustur</button>
+        <button type='submit' class='btn btn-primary' name=gonder> SUBMIT</button>
     </form>";
 }
-    //detele
-function sil()
-{
-    $c = mysqli_connect('localhost', 'root', '', 'okul'); // VT'ye baglan
-    $sql = "DELETE FROM ogrenci WHERE ono=" . $_GET['no'] . ";";
-    $result = mysqli_query($c, $sql); // SQL komutunu calistir
-    if (!$result) // komutu calistirirken hata olustumu?
-    echo "SQL error:" . mysqli_error($c);
-    mysqli_close($c); // VT baglantisini kapat
-    //header("location:index.php"); // browser'in ogrenci.php sayfasini yuklemesini sagla
-}
-   //checking if the student no is exist or not 
-function checking()
+//student list
+function listed()
 {
     $c = mysqli_connect('localhost', 'root', '', 'okul');
-    if (isset($_GET['no']) && isset($_GET['ad']) && isset($_POST['soyad']));
-    {
-        $no = $_GET['no'];
-        $ad = $_GET['ad'];
-        $soyad = $_GET['soyad'];
-
-        $sql = "SELECT* FROM ogrenci where ono = '$no' ";
-        $result = mysqli_query($c, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            echo 'Username already Exists!!';
-        } else {
-            ekle();
-        }
-    }
-}
-    //list after adding the student
-function listele()
-{
-    $c = mysqli_connect('localhost', 'root', '', 'okul');
-     $sql = "SELECT * FROM ogrenci ";
-    
+    $result_per_page = 5;
+    $sql = "SELECT * FROM ogrenci ";
+    $result = mysqli_query($c, $sql);
+    $number_of_result = mysqli_num_rows($result);
     echo "<br>
     <div class='table-responsive col-md-4 align-middle'>
         <h2>Ogrenci listesi</h2>
@@ -187,24 +183,19 @@ function listele()
             <th scope='col'>
                     <a href='?orderBy=soyadi'>Soyadi</a>
             </th>
-            <td>Sil</td>
+            <td>detele</td>
             <td>Degistir</td>
         </tr>
         </thead>
         <br>
         ";
-        //sorting the variables 
-  /*  $orderBy = array('no', 'adi', 'soyadi');//Start
+         //sorting the variables 
+    $orderBy = array('no', 'adi', 'soyadi');//Start
     $order = 'type';
     if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
         $order = $_GET['orderBy'];
         $sql = "SELECT * FROM ogrenci ORDER BY " . $order;
     }//end 
-*/
-    $result_per_page = 6; // number of name you want to show in list
-    $result1 = mysqli_query($c, $sql);
-    $number_of_result = mysqli_num_rows($result1); // number of result
-   
     $number_of_pages = ceil($number_of_result / $result_per_page);
 //which page the visitor is 
     if (!isset($_GET['page'])) {
@@ -212,35 +203,36 @@ function listele()
     } else {
         $page = $_GET['page'];
     }
-
-    $this_page_first_result = ($page - 1) * $result_per_page; //determine limit starting number for the result
+    $this_page_first_result = ($page - 1) * $result_per_page;
     $sql = "SELECT * FROM ogrenci  LIMIT " . $this_page_first_result . ',' . $result_per_page;
-    $result1 = mysqli_query($c, $sql);
+    $result = mysqli_query($c, $sql);
 
-    while ($row = mysqli_fetch_array($result1)) {
+    while ($row = mysqli_fetch_array($result)) {
         echo "
-        <tr>
-            <td>" . $row[0] . "</td>
-            <td>" . $row[1] . "</td>
-            <td>" . $row[2] . "</td>
-            <td><a href='?is=sil&no=" . $row[0] . "'>Sil</a></td>
-            <td><a href='?is=degistir&no={$row[0]}&ad={$row[1]}&soyad={$row[2]}'>Degistir</a></td>
-        </tr>";
+    <tr>
+        <td>" . $row[0] . "</td>
+        <td>" . $row[1] . "</td>
+        <td>" . $row[2] . "</td>
+        <td><a href='?is=detele&no=" . $row[0] . "'>detele</a></td>
+        <td><a href='?is=degistir&no={$row[0]}&ad={$row[1]}&soyad={$row[2]}'>Degistir</a></td>
+    </tr> <br>";
     }
-    ?>
-        </table> 
-     <?php  //display the links to the page
+
     for ($page = 1; $page <= $number_of_pages; $page++) {
         echo '<a href="page.php?page=' . $page . '">' . $page . '</a>';
-    } ?>  <br> 
-    <a href='?is=yeni' class='btn btn-primary btn-lg active' role='button' aria-pressed='true'>Yeni ogrenci</a></div>
+    }
+    ?>
+ </table>
+ <br><br> 
+    <a href='?is=newStd' class='btn btn-primary btn-lg active' role='button' aria-pressed='true'>NEW STUDENT</a></div>
     <?php
     mysqli_close($c);
 }
 ?>
-    <!-- Optional JavaScript -->
+     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+</html>
