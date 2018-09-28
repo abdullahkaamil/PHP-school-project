b00df3e023f3272f7295b6ad12c9eceb91631940
akaam <akaamil@outlook.com>
2018-09-28 14:56:40 +0300

    new style pagination

--- page.php
+++ page.php
@@ -1,17 +1,17 @@
 <html>
 <head>
+<link href="../PHP-school-project/css/pagination.css" rel="stylesheet" type="text/css" />
+<link href="../PHP-school-project/css/A_green.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
  <title>MY first PHP project</title> </head>
  <body>
-<?php 
-
-function connect(){
+<?php
+require("../PHP-school-project/function.php");
+function connect()
+{
     return mysqli_connect('localhost', 'root', '', 'okul'); //sql connect
-
 }
 
-echo "
-<pre>";print_r($_GET); echo "</pre>";
 //switching between fucntions
 if (!isset($_GET['is']))
     $_GET['is'] = '';
@@ -19,9 +19,6 @@ switch ($_GET['is']) {
     case 'detele':
         detele();
         listed();
-        break;
-    case 'newStd':
-        newStd();
         break;
     case 'add':
         checking();
@@ -40,18 +37,17 @@ switch ($_GET['is']) {
 // update
 function update()
 {
-    
+
     $sql = "UPDATE ogrenci SET adi='{$_GET['ad']}',soyadi='{$_GET['soyad']}' WHERE ono={$_GET['no']};"; //sql update query
     $result = mysqli_query($c, $sql);
     if (mysqli_affected_rows($c) == 0)
         echo "Kayit bulunamadi (no={$_GET['no']})";
     mysqli_close($c);
 }
- //checking if the student no is exist or not 
+ //checking if the student no is exist or not
 function checking()
 {
     $c = connect();
-  //  $c = mysqli_connect('localhost', 'root', '', 'okul');
     if (isset($_GET['no']) && isset($_GET['ad']) && isset($_POST['soyad']));
     {
         $no = $_GET['no'];
@@ -102,86 +98,34 @@ function form()
 function add()
 {
     $c = connect();
-    //$c = mysqli_connect('localhost', 'root', '', 'okul');
     $sql = "INSERT INTO ogrenci(ono,adi,soyadi) VALUES(" . $_GET['no'] . ",'" . $_GET['ad'] . "','" . $_GET['soyad'] . "');";
-    echo $sql . "<br>";
+    //echo $sql . "<br>";
     $result = mysqli_query($c, $sql) or mysqli_error($c);
-    echo $result;
+   // echo $result;
     mysqli_close($c);
 }
  //detele
 function detele()
 {
     $c = connect();
-
-    //$c = mysqli_connect('localhost', 'root', '', 'okul'); // VT'ye baglan
     $sql = "DELETE FROM ogrenci WHERE ono=" . $_GET['no'] . ";";
     $result = mysqli_query($c, $sql); // SQL komutunu calistir
     if (!$result) // komutu calistirirken hata olustumu?
     echo "SQL error:" . mysqli_error($c);
     mysqli_close($c); // VT baglantisini kapat
-     //header("location:index.php"); // browser'in ogrenci.php sayfasini yuklemesini sagla
-}
-// 
-function newStd()
-{
-    echo "
-    <form>
-        NEW STUDENT
-        <input type=hidden name=is value=add>
-        <div class='form-group row col-md-8'>
-            <label for='inputno' class='col-sm-3 col-form-label'>
-                NO: </lable>
-                <div class='col-sm-10'>
-                    <input name=no type=text>
-                </div>
-        </div>
-
-        <div class='form-group row col-md-8'>
-            <label for='inputno' class='col-sm-3 col-form-label'>
-                AD: </lable>
-                <div class='col-sm-10'>
-                    <input name=ad type=text>
-                </div>
-        </div>
-
-        <div class='form-group row col-md-8'>
-            <label for='inputno' class='col-sm-3 col-form-label'> SOYAD: </lable>
-                <div class='col-sm-10'>
-                    <input name=soyad type=text>
-                </div>
-        </div>
-
-        <fieldset class='form-group'>
-            <div class='row'>
-                <legend class='col-form-label col-sm-2 pt-0'>Censiyet</legend>
-                <div class='col-sm-5'>
-                    <div class='form-check'>
-                        <input type=radio name=cinsiyet value=E>
-                        <label class='form-check-label' for='gridRadios1'>   Erkek </label>
-                    </div>
-                    <div class='form-check'>
-                        <input type=radio name=cinsiyet value=K>
-                        <label class='form-check-label' for='gridRadios1'> Kadin </lable>
-                    </div>
-                </div>
-            </div>
-        </fieldset>
-        <button type='submit' class='btn btn-primary' name=gonder> SUBMIT</button>
-    </form>";
 }
 //student list
 function listed()
 {
+
     $c = connect();
-
-   // $c = mysqli_connect('localhost', 'root', '', 'okul');
-    $result_per_page = 5;
+   
+    //$result_per_page = 5;
     $sql = "SELECT * FROM ogrenci ";
-    $result = mysqli_query($c, $sql);
-    $number_of_result = mysqli_num_rows($result);
+    //$result = mysqli_query($c, $sql);
+  //  $number_of_result = mysqli_num_rows($result);
 
-// sorting the arrays 
+// sorting the arrays
 
     if (isset($_GET['order'])) {
         $order = $_GET['order'];
@@ -191,36 +135,34 @@ function listed()
     if (isset($_GET['sort'])) {
         $sort = $_GET['sort'];
     } else {
-        $sort = 'ASC';
+        $sort = 'DESC';
     }
     if (isset($_GET['sortso'])) {
         $sort1 = $_GET['sortso'];
     } else {
         $sort1 = 'soyadi';
     }
-  //  $resultset = $mysqli->query("SELECT * FROM ogrenci  ORDER by $order $sort $sort1");
-  $c = connect();
- 
-  // $sql = "SELECT * FROM ogrenci order by $order $sort ";
+    $c = connect();   
     $result = mysqli_query($c, $sql);
     if ($result->num_rows > 0) {
-        $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
+        $sort == 'ASC' ? $sort = 'DESC' : $sort = 'ASC';
         while ($rows = $result->fetch_assoc()) {
             $ono = $rows['ono'];
             $adi = $rows['adi'];
             $soyadi = $rows['soyadi'];
         }
     }
-    echo "<br>
-    <div class='table-responsive col-md-4 align-middle'>
+    echo "
+    <div class='table-responsive col-md-9 align-middle'>
         <h2>Student List</h2>
+        <form>
         <table class='table table-striped'>
         <thead>
         <tr>
             <th scope='col'>
                    <a href='?order=ono&&sort=$sort'>No</a>
             </th>
-            <th scope='col'> 
+            <th scope='col'>
                     <a href='?order=adi&&sort=$sort'>Adi</a>
             </th>
             <th scope='col'>
@@ -230,54 +172,58 @@ function listed()
             <td>UpDate</td>
         </tr>
         </thead>
+        <tr>
+        <input type=hidden name=is value=add>
+       <td>  <input name=no type=text></td>
+       <td>  <input name=ad type=text></td>
+       <td>  <input name=soyad type=text></td>
+       <td> <button type='submit' class='btn btn-primary' name=gonder> SAVE</button> </td>
+       <td>&nbsp;</td>
         <br>";
-        /* //sorting the variables 
-    $orderBy = array('no', 'adi', 'soyadi');//Start
-    $order = 'type';
-    if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
-        $order = $_GET['orderBy'];
-        $sql = "SELECT * FROM ogrenci ORDER BY " . $order;
-    }//end */
-    $number_of_pages = ceil($number_of_result / $result_per_page);
-//which page the visitor is 
-    if (!isset($_GET['page'])) {
-        $page = 1;
-    } else {
-        $page = $_GET['page'];
-    }
-    $this_page_first_result = ($page - 1) * $result_per_page;
-    $sql = "SELECT * FROM ogrenci  order by $order $sort  LIMIT " . $this_page_first_result . ',' . $result_per_page;
-  //  echo ("SELECT * FROM ogrenci  order by $order $sort  LIMIT " . $this_page_first_result . ',' . $result_per_page);
+    
+    $c = connect(); 
+    $page = (int)(!isset($_GET["page"]) ? 1 : $_GET["page"]);
+    $limit = 5;
+         $startpoint = ($page * $limit) - $limit; 
+    $sql = "SELECT * FROM ogrenci  order by $order $sort LIMIT " . $startpoint . ',' .$limit;
     $result = mysqli_query($c, $sql);
+   // var_dump($result); die;
     while ($row = mysqli_fetch_array($result)) {
         $ono = $row['ono'];
         $adi = $row['adi'];
         $soyadi = $row['soyadi'];
-        echo "
+       echo "
     <tr>
         <td>" . $ono . "</td>
         <td>" . $adi . "</td>
         <td>" . $soyadi . "</td>
-        <td><a href='?is=detele&no=" . $ono . "'>Detele</a></td>
+        <td><a href='?is=detele&no=" . $ono . "'>Delete</a></td>
         <td><a href='?is=degistir&no={$ono}&ad={$adi}&soyad={$soyadi}'>Update</a></td>
     </tr> <br>";
     }
     ?>
+    </form>
     </table>
- <?php 
-for ($page = 1; $page <= $number_of_pages; $page++) {
-    echo '<a href="page.php?page=' . $page . '">' . $page . '</a>';
-} ?>
-    <a href='?is=newStd' class='btn btn-primary btn-lg active' role='button' aria-pressed='true'>NEW STUDENT</a>
+
+ 
+
 </div>
-    <?php
+    
+<?php 
+
+echo "<div id='pagingg' >";
+
+echo pagination($sql, $limit, $page);
+echo "</div>";
+
     mysqli_close($c);
 }
 ?>
-     <!-- Optional JavaScript -->
+  </body>  
+   <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
+
 </html>
